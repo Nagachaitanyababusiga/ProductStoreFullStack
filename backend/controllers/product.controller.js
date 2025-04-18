0a7c0e7c87377a00f41929dc4b4e180f491d3551
S.Nagachaitanyababu <nagachaitanyababusiga@gmail.com>
2025-04-19 05:28:27 +0530

    adding files upto now

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -60,3 +60,4 @@ export const deleteProduct=async(req,res)=>{
         res.status(500).json({success:false,message:"Server Error"});
     }
 }
+
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -21,3 +21,4 @@ router.delete("/:id",deleteProduct);
 
 
 export default router;
+
